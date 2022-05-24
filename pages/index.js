01c440b07db64a11d7f80c02c07db924b00c23ac
B0AY-C0D3GR0UND <99433641+B0AY-C0D3GR0UND@users.noreply.github.com>
2022-05-25 00:06:50 +0500

    Property components working

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import {Flex,Box,Text,Button} from '@chakra-ui/react'
+import { Flex, Box, Text, Button } from '@chakra-ui/react'
 
-import { baseUrl,fetchApi } from '../utils/fetchApi';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 import Property from '../components/Property';
 
-const Banner = ({purpose,title1,title2,desc1,desc2,buttonText,linkName,imageUrl})=>(
+const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src={imageUrl} width={500} height={300} alt="banner"/>
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
     <Box p="5">
       <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
       <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
@@ -20,48 +20,50 @@ const Banner = ({purpose,title1,title2,desc1,desc2,buttonText,linkName,imageUrl}
   </Flex>
 )
 
-export default function Home({propertyForSale,propertyForRent}) {
+export default function Home({ propertyForSale, propertyForRent }) {
   // console.log(propertyForSale,propertyForRent);
   return (
     <Box>
-    <Banner 
-      purpose="RENT A HOME"
-      title1="Rental Homes for"
-      title2="Everyone"
-      desc1="Explore Apartments, Villas , Homes"
-      desc2="and more"
-      buttonText="Explore Renting"
-      linkName="/search?purpose=for-rent"
-      imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-    />
-    <Flex flexWrap="wrap">
-      {propertyForRent.map((property)=><Property property={property} key={property.id}/>)}
-    </Flex>
-    <Banner   
-      purpose="BUY A HOME"
-      title1="Find, Buy and Own Your"
-      title2="Dream Home"
-      desc1="Explore Apartments, Villas , Homes"
-      desc2="and more"
-      buttonText="Explore Buying"
-      linkName="/search?purpose=for-sale"
-      imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-    />
-      {propertyForSale.map((property)=><Property property={property} key={property.id}/>)}
+      <Banner
+        purpose="RENT A HOME"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas , Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      <Flex flexWrap="wrap">
+        {propertyForRent.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
+      <Banner
+        purpose="BUY A HOME"
+        title1="Find, Buy and Own Your"
+        title2="Dream Home"
+        desc1="Explore Apartments, Villas , Homes"
+        desc2="and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      />
 
+      <Flex flexWrap="wrap">
+        {propertyForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
     </Box>
   );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
 
   const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
   const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
 
   return {
-    props:{
-      propertyForSale:propertyForSale?.hits,
-      propertyForRent:propertyForRent?.hits,
+    props: {
+      propertyForSale: propertyForSale?.hits,
+      propertyForRent: propertyForRent?.hits,
     }
   }
 }
